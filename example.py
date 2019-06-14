b4d7383b58b8c1b5d370e278e787d274ba03c9d8
plaflin <patricklaflin@gmail.com>
2019-06-13 20:48:31 -0400

    Automate the Boring Stuff Day 2
    Not as much as Day 1 but still did something

---
+++ example.py
@@ -0,0 +1,14 @@
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+
+hello()
+hello()
+hello()
+
+def plusOne(number):
+    return number + 1
+
+newNumber = plusOne(5)
+print(newNumber)
